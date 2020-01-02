7fbe21eeabda938c78d615362d045567176efd3d
Ilya Neron <ilyaneron@gmail.com>
2020-01-02 14:37:13 +0200

    [Implement] Added typescript support (#6)
    * Added typescript support
    
    * Added ts config
    
    Co-authored-by: VadimKorobka <34455@mail.ua>

--- src/renderer/components/App.svelte
+++ src/renderer/components/App.svelte
@@ -1,10 +1,11 @@
-<script>
-    export let name;
+<script lang="typescript">
+    export const name: string = 'World';
 </script>
 
 <style lang="scss">
     h1 {
         text-align: center;
+        font-size: 25px;
     }
 </style>
 
