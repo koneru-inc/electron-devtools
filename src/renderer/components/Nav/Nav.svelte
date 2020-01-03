907c464e38b923c6159b6612dab7aba1996527d2
Neron Ilya <34775579+IlyaNeron@users.noreply.github.com>
2020-01-03 18:19:06 +0200

    [Implement] Router, base components (#10)
    * Added router, implement base components
    
    * fix conflicts

---
+++ src/renderer/components/Nav/Nav.svelte
@@ -0,0 +1,9 @@
+<script>
+    import { link } from 'svelte-spa-router'
+
+    const navItems = ['Console', 'Network', 'Api', 'Rpc']
+</script>
+
+{#each navItems as item}
+    <a href="/{item}" use:link>{item}</a>
+{/each}
