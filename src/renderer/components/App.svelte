573815b44854d9408b639add877c0bc0f1da8ccd
Vadim Korobka <34455@mail.ua>
2020-01-18 22:37:47 +0200

    [Implement] Send events to devtools

--- src/renderer/components/App.svelte
+++ src/renderer/components/App.svelte
@@ -2,6 +2,10 @@
     import Header from './Header/Header'
     import Main from './Main/Main.svelte'
     import Footer from './Footer/Footer'
+    const { ipcRenderer } = __non_webpack_require__('electron')
+    ipcRenderer.on('@ELECTRON_DEVTOOLS/CONSOLE/log', (event, ...args) => {
+        console.log(...args);
+    })
 </script>
 
 <style lang="scss">
